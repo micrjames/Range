653165200a91e815349c1d08b24d059a456f3cda
Michael James <micrjames921@gmail.com>
2024-01-06 20:13:36 -0700

    set up tests

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,5 +1,8 @@
-const { range } = require("./range");
+const { range } = require("../range");
 
 const nums = [...range(5)];
+describe("Range operator.", () => {
+   test.todo("Should be a sequence of 5 numbers.");
+   test.todo("Should output a sequence of the first 5 numbers.");
+});
 
-console.log(nums);
